6e407cd1e506364e5fb57240884362c9c4ceb501
Mousumi <dutta.mousumi136@gmail.com>
2024-02-15 21:00:40 -0500

    event emitting by using @Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,21 @@ export class AppComponent {
   // newServerName = '';
   // newServerContent = '';
 
+  onServerAdded(serverData: {serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
 
 }
